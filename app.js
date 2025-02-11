b7c01b7d2b3badc6e7234469b732a59466e89c81
DevRudy <55453823+SketchRudy@users.noreply.github.com>
2025-02-10 18:39:11 -0800

    Added admin routing and syling for confirmation page

--- app.js
+++ app.js
@@ -3,14 +3,37 @@ import express from  'express';
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+const orders = []; // In a real app, we'd use a database
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(`${import.meta.dirname}/views/home.html`);
 });
 
-// app.get('/test', (req, res) => {
-//     res.send("Different text");
-// });
+app.post('/submit-order', (req, res) => {
+    // Get form data from request body
+    const order = {
+       name: req.body.name,
+       email: req.body.email,
+       message: req.body.message,
+       event: req.body.event,
+       timestamp: new Date()
+    };
+    
+    // Save order to our array
+    orders.push(order);
+ 
+    // Log the orders array to the console
+    console.log(orders);
+    
+    // Send confirmation page
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+ });
+
+ app.get('/admin/orders', (req, res) => {
+    res.send(orders);
+});
+
 
 app.listen(3000, () => {
     console.log("Server started on http://localhost:3000");
